30c98b2249b1a0f12e95305167373221757fbf66
Nattachai928 <138751701+Nattachai928@users.noreply.github.com>
2023-07-13 15:57:18 +0700

    Update to Remote LabHw

---
+++ menu.php
@@ -0,0 +1,5 @@
+<?php echo "MR.NATTACHAI SROISUNGNAE"; ?>
+<?php echo "STUDENTNUMBER : 6411130017"; ?>
+<?php echo "MAJOR : ENGINEERING COMPUTER"; ?>
+<?php echo "TELL : 88888888888"; ?>
+<?php echo "I LINK CAT"; ?>
