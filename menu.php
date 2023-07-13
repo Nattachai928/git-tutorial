5600eeaa735731727d8f2011364e4886d3726774
Nutxu <138751701+Nattachai928@users.noreply.github.com>
2023-07-13 16:17:25 +0700

    Delete menu.php

--- menu.php
+++
@@ -1,7 +0,0 @@
-<?php echo "MR.NATTACHAI SROISUNGNAE"; ?>
-<?php echo "STUDENTNUMBER : 6411130017"; ?>
-<?php echo "MAJOR : ENGINEERING COMPUTER"; ?>
-<?php echo "TELL : 88888888888"; ?>
-<?php echo "I LINK CAT"; ?>
-<?php echo " MAHANAKORN "; ?>
-<?php echo " TECNOLOGY "; ?>
